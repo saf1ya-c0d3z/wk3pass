af56184f9f0ea963e0bef0248fb1755693b187e6
Zoe-Sophia <zoe-sophia@Zoe-Sophias-MacBook-Pro.local>
2022-12-21 00:17:32 -0800

    Added functions for passwordgen

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
-// Create a function to make guidlines for password
+var characters ='abcdefghijklmnopqrstuvwxyz';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  }
+
+console.log(generateString(5));
 function generatePassword() {
   //define Variable for WHEN prompted for the length of the password (
-  var passlength = prompt("Enter a passowrd length between 8-128 characters,");
-  //define Variable for length of at least 8 characters and no more than 128 characters
+  var passlength = prompt("Enter a number for your passowrd length between 8-128 characters,");
   //define Variable for character types to include in the password
-  //variable lowercase, 
-  // variable uppercase, 
-  // variable numeric
-  // variable special characters
-  // Make Prompt for buttom
+ 
   if (passlength < 8 || passlength > 128) {
     alert("You're password is too short or too long try again.")
     return ""
   }
 var lowercaseq = confirm ("Do you want to include Lowercase letters?");
- var uppercaseq = confirm (" Do you want to use Uppercase letters?")
+ var uppercaseq = confirm (" Do you want to include Uppercase letters?");
+ var numberq = confirm ("Would you like to use numbers?");
+ var specialchar = confirm ("Would you like to use special characters? ex: *, $, ?, !, @ ect.");
+
+ if (!lowercaseq && !uppercaseq && !numberq && !specialchar) {
+  alert ("Sorry, you must select at least one of these choices, please try again.");
+  generatePassword()
+
+ }
+if (lowercaseq) {
+ var password = generateString (passlength); 
+
 }
+if (numberq){ 
+  password= password.replace(password.charAt(0),3);
+  
+} 
+return password
+
+}
+
 
 
 
@@ -29,6 +55,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generateLowercaseChar() {
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
