10f1541ccb193ad66844a754e5008afa653726fa
Zoe-Sophia <zoe-sophia@Zoe-Sophias-MacBook-Pro.local>
2022-12-22 21:48:34 -0800

    finalized password generator

--- script.js
+++ script.js
@@ -1,86 +1,83 @@
-
 var generateBtn = document.querySelector("#generate");
 var characters ='abcdefghijklmnopqrstuvwxyz';
-var Ulcharacters ='nopqrstuvwxyzABCDEFGHIJKL';
-var LSCcharacters ='nopqvwxyzABCIJKL*&$#@?%!+~';
-function generateString(length) {
-    let result = ' ';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result;
-  }
-  function UlgenerateString(length) {
-    let result = ' ';
-    const charactersLength = Ulcharacters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += Ulcharacters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result;
-  }
-  function LSCgenerateString(length) {
-    let result = ' ';
-    const charactersLength = LSCcharacters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += LSCcharacters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result;
-  }
-console.log(generateString(5));
-function generatePassword() {
-  //define Variable for WHEN prompted for the length of the password (
-  var passlength = prompt("Enter a number for your passowrd length between 8-128 characters,");
-  //define Variable for character types to include in the password
+var Ulcharacters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var LSCcharacters ='*&$#@?%!+~';
+var numbers = "1234567890"
+var combinechars = ""
+let result = ' ';
  
-  if (passlength < 8 || passlength > 128) {
-    alert("You're password is too short or too long try again.")
-    return ""
-  }
-var lowercaseq = confirm ("Do you want to include Lowercase letters?");
- var uppercaseq = confirm (" Do you want to include Uppercase letters?");
- var numberq = confirm ("Would you like to use numbers?");
- var specialchar = confirm ("Would you like to use special characters? ex: *, $, ?, !, @ ect.");
-
- if (!lowercaseq && !uppercaseq && !numberq && !specialchar) {
-  alert ("Sorry, you must select at least one of these choices, please try again.");
-  generatePassword()
-
+function generatePassword(passlength, combinechars, initialpassword) {
+  let result = initialpassword;
+  //const charactersLength = combinechars.passlength;
+ for ( let i = initialpassword.length; i < passlength; i++ ) {
+     result += combinechars.charAt(Math.floor(Math.random() * combinechars.length));
+   }
+ 
+   return result;
  }
-if (lowercaseq) {
- var password = generateString (passlength); 
+ 
+ 
+ 
+function generateString() {
+ //define Variable for WHEN prompted for the length of the password (
+ var passlength = prompt("Enter a number for your passowrd length between 8-128 characters,");
+ //define Variable for character types to include in the password
+ if (passlength < 8 || passlength > 128) {
+   alert("You're password is too short or too long try again.")
+   return ""
+ }
+var lowercaseq = confirm ("Do you want to include Lowercase letters?");
+var uppercaseq = confirm (" Do you want to include Uppercase letters?");
+var numberq = confirm ("Would you like to use numbers?");
+var specialchar = confirm ("Would you like to use special characters? ex: *, $, ?, !, @ ect.");
+ 
+if (!lowercaseq && !uppercaseq && !numberq && !specialchar) {
+ alert ("Sorry, you must select at least one of these choices, please try again.");
+ //generatePassword()
+ 
 
 }
-if (numberq){ 
-  password= password.replace(password.charAt(1),6);
-  
-} 
+var initialpassword = ""
+if (lowercaseq) {
+combinechars += characters;
+initialpassword +=  characters.charAt(Math.floor(Math.random() * characters.length));
+}
+if (numberq){
+combinechars += numbers;
+initialpassword +=  numbers.charAt(Math.floor(Math.random() * numbers.length));
+ }
 if (uppercaseq) {
-  password = UlgenerateString (passlength);
+combinechars += Ulcharacters;
+initialpassword +=  Ulcharacters.charAt(Math.floor(Math.random() * Ulcharacters.length));
 }
 if (specialchar) {
-  password = LSCgenerateString (passlength);
+ combinechars += LSCcharacters;
+ initialpassword +=  LSCcharacters.charAt(Math.floor(Math.random() * LSCcharacters.length));
+ }
+ console.log(combinechars)
+
+var result = generatePassword(passlength, combinechars, initialpassword );
+//writePassword(password)
+return result ;
+ 
 }
-return password
-
-}
-
-
-
-
+ 
+ 
+ 
+ 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+ var password = generateString();
+ var passwordText = document.querySelector("#password");
+ 
+ passwordText.value = password;
+ 
 }
-function generateLowercaseChar() {
-
-}
-// Add event listener to generate button
+//function generateLowercaseChar() {
+ 
+//}
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ 
+
+
